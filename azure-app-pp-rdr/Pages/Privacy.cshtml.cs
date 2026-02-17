a96c21606bb6fda74c4714b539a5dadac0fcc437
op2 <op2@pc-debian.localdomain>
2026-02-17 17:07:55 +0100

    Initial commit

---
+++ azure-app-pp-rdr/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_pp_rdr.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
