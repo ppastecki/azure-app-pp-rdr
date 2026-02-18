198c6aaf1c8abd80a7560edfcc820ccd6008e2e2
Patryk <patryk@acme.com>
2026-02-18 12:02:00 +0100

    Add a greeting

--- azure-app-pp-rdr/Pages/Index.cshtml.cs
+++ azure-app-pp-rdr/Pages/Index.cshtml.cs
@@ -5,7 +5,15 @@ namespace azure_app_pp_rdr.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly IConfiguration _configuration;
+
+    public IndexModel(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public void OnGet()
     {
+        ViewData["Greeting"] = _configuration["Greeting"];
     }
 }
